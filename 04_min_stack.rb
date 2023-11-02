7570530ecc4d78bcd60e99a44b3a031d142e4b64
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-11-02 16:59:15 -0400

    Add code for min stack

---
+++ 04_min_stack.rb
@@ -0,0 +1,49 @@
+class MinStack
+  def initialize
+    @main = []
+    @min = []
+  end
+
+  def push(n)
+    if @main.length == 0
+      @min << n
+    elsif n <= @min[-1]
+      @min << n
+    else
+      @min << @min[-1]
+    end
+
+    @main << n
+  end
+
+  def pop
+    @min.pop
+    @main.pop # returns this line
+  end
+
+  def get_min
+    @min[-1]
+  end
+end
+
+# The original Python code:
+# class MinStack():
+#   def __init__(self):
+#     self.main = []
+#     self.min = []
+
+#   def push(self, n):
+#     if len(self.main) == 0:
+#       self.min.append(n)
+#     elif n <= self.min[-1]:
+#       self.min.append(n)
+#     else:
+#       self.min.append(self.min[-1])
+#     self.main.append(n)
+
+#   def pop(self):
+#     self.min.pop()
+#     return self.main.pop()
+
+#   def get_min(self):
+#     return self.min[-1]
